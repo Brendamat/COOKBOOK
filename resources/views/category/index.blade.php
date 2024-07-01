31fb96e680c7041dd5820cdf4b8fbd20652c80a6
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2024-06-30 22:50:40 -0300

    Ajustes gerais da opçao de categorias

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,27 +8,27 @@
     @if(session('error'))
         <p>{{session('error')}}</p>
     @endif
-    <div class="formulario">
-    @foreach ($categories as $category)
-        <div class="listagem">
-            <h3>{{ $category->name }}</h3>
+    <div class="formulario-foreach">
+        @foreach ($categories as $category)
+            <div class="listagem">
+                <h3>{{ $category->name }}</h3>
 
-            <div class="d-flex align-items-center">
-                <form action="{{route('category.update')}}" method="POST" class="form-inline">
-                    @csrf
-                    <input type="hidden" name="id" value="{{$category->id}}">
-                    <input type="text" name="name" placeholder="Atualizar nome da categoria">
-                    <button type="submit" class="salvar">Atualizar</button>
-                </form>
+                <div class="d-flex align-items-center">
+                    <form action="{{route('category.update')}}" method="POST" class="form-inline">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$category->id}}">
+                        <input type="text" name="name" placeholder="Atualizar nome da categoria">
+                        <button type="submit" class="salvar">Atualizar</button>
+                    </form>
 
-                <form action="{{route('category.delete')}}" method="POST" class="form-inline">
-                    @csrf
-                    <input type="hidden" name="id" value="{{$category->id}}">
-                    <button type="submit" class="deletar">Deletar</button>
-                </form>
+                    <form action="{{route('category.delete')}}" method="POST" class="form-inline">
+                        @csrf
+                        <input type="hidden" name="id" value="{{$category->id}}">
+                        <button type="submit" class="deletar">Deletar</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
     </div>
     <h1>Cadastrar categoria</h1>
 
@@ -41,6 +41,11 @@
 
 @section('style')
 <style>
+    main {
+        background-color: #FBF7ED;
+        height: 100vh;
+    }
+
     .salvar, .deletar {
         background-color: #FBF7ED;
         border: 1px solid #FF9E0B;
@@ -50,44 +55,56 @@
         padding: 12px 18px;
         margin-left: 10px; /* Espaçamento entre os botões */
     }
+
+    .salvar { margin-left: 0; }
+
     .deletar {
         background-color: #fbeded;
         border: 1px solid #a2363b;
         color: #e41313;
         padding: 12px 16px;
     }
-    main {
-        background-color: #FBF7ED;
-        height: 100vh;
-    }
+
     .box-categorias {
         padding: 120px 100px;
     }
+
     .box-categoriasWrapper {
         background-color: #fff;
         border: 1px solid #FF9E0B;
         padding: 30px;
         border-radius: 16px;
     }
+
     .box-categoriasWrapper h1 {
         font-size: 28px;
         color: #8E3F1A;
     }
+
     .box-categoriasWrapper table {
         width: 100%;
     }
+
     .box-categoriasWrapper thead {
         background-color: #FF9E0B;
     }
+
     .box-categoriasWrapper th {
         padding: 8px;
         color: #FBF7ED;
     }
+
     .box-categoriasWrapper td {
         padding: 10px;
         border-bottom: 1px solid #FF9E0B;
     }
-    .formulario form > input, .form-inline > input {
+
+    .formulario-foreach {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    .form-inline > input {
         background-color: #fff;
         border-radius: 4px;
         height: 48px;
@@ -97,34 +114,49 @@
         font-weight: 400;
         border: 1px solid rgba(234, 195, 157, .5);
         font-size: 15px;
+        margin-right: 10px;
     }
+
     .formulario {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
+        display: flex;
+        flex-direction: column;
         padding: 20px;
+        gap: 20px;
     }
+
+    .formulario > input {
+        padding: 10px;
+        border: 1px solid rgba(234, 195, 157, .5);
+        border-radius: 4px;
+    }
+
     h1 {
         color: #FF9E0B;
         font-size: 36px;
         font-weight: 500;
         letter-spacing: normal;
         line-height: 120%;
-        padding: 20px 0px 20px 50px;
+        padding: 20px 0px 20px 20px;
     }
+
     h3 {
         font-size: 1.3rem;
         display: inline-block; /* Mantém o título na mesma linha */
-        margin-right: 10px;
+        padding: 0 14px;
     }
+
     .listagem {
         margin-bottom: 50px;
     }
+
     .d-flex {
         display: flex;
     }
+
     .align-items-center {
         align-items: center;
     }
+
     .form-inline {
         display: flex;
         align-items: center;
